ae72506a8d7714c08695a1b5af2697be8917c54b
Angie <angiegomez.9924@gmail.com>
2020-12-12 16:11:43 -0500

    Interfaz Login y Home

--- src/components/auth/TheLogin.vue
+++ src/components/auth/TheLogin.vue
@@ -1,56 +1,120 @@
 <template>
-    <form>
-        <div class="form-group">
-            <label for="exampleInputUser1">User</label>
-            <input v-model="login.email" type="email" class="form-control" id="exampleInputUser1">
+  <div >
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <img
+              class="img-login"
+              :src="require('../../../public/resources/img-login.png')"
+            />
+          </div>
+          <div class="col-6">
+            <form class="form">
+              <div class="form-group">
+                <img
+                  class="icon-user"
+                  :src="require('../../../public/resources/user.png')"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputUser1">User</label>
+                <input
+                  v-model="login.email"
+                  type="email"
+                  class="form-control"
+                  id="exampleInputUser1"
+                />
+              </div>
+              <div class="form-group">
+                <label  for="exampleInputPassword1">Password</label>
+                <input
+                  v-model="login.password"
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <button
+                @click.prevent="LoginUser"
+                type="submit"
+                class="btn btn-light btn-sm"
+              >
+                Login
+              </button>
+              <!-- <pre>{{$data}}</pre> -->
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input v-model="login.password" type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button @click.prevent= "LoginUser" type="submit" class="btn btn-primary">Login</button>
-    <!-- <pre>{{$data}}</pre> -->
-    </form>
+      </div>
+  </div>
 </template>
 
 <script>
-import swal from 'sweetalert'
+import swal from "sweetalert";
 export default {
-    data(){
-        return{
-            login:{
-                user:'',
-                password:''
-            },
+  data() {
+    return {
+      login: {
+        user: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async LoginUser() {
+      try {
+        let response = await this.$http.post("/api/usuario/login", this.login);
+        console.log(response.data);
+
+        // ¡¡¡¡Cambio aquí!!!! tokenReturn por el nombre del token y user por el nombre del usuario en nuestra DB.
+        let token = response.data.tokenReturn;
+        let userData = response.data.user;
+
+        localStorage.setItem("jwt", token);
+        localStorage.setItem("user", JSON.stringify(userData));
+
+        if (token) {
+          this.$router.push("/home");
         }
+        swal("Great!", "Welcome back :D", "success");
+      } catch (error) {
+        console.log(error);
+        swal("Oops!", "Incorrect username or password D:", "error");
+      }
     },
-    methods:{
-        async LoginUser(){
-            try{
-                let response= await this.$http.post('/api/usuario/login', this.login);
-                console.log(response.data);
-
-                // ¡¡¡¡Cambio aquí!!!! tokenReturn por el nombre del token y user por el nombre del usuario en nuestra DB.
-                let token= response.data.tokenReturn;
-                let userData= response.data.user;
-
-                localStorage.setItem('jwt', token);
-                localStorage.setItem('user', JSON.stringify(userData));
-
-                if(token){
-                    this.$router.push('/home');
-                }
-                swal('Great!', "Welcome back :D", 'success');
-            }
-            catch(error){
-                console.log(error);
-                swal('Oops!', 'Incorrect username or password D:', 'error');
-            }
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
+.container {
+margin-top: 100px;
+  background-color:#73C6B6;
+  opacity: .7;
+  width: 800px;
+  border-radius: 5px;
+}
+.form {
+  padding: 70px;
+  text-align: center;
+}
+
+.img-login {
+  width: 100%;
+  margin-top: 20px;
+  margin-left: 35px;
+}
+
+.col-6 {
+  height: 500px;
+}
+
+.icon-user {
+  width: 100px;
+  height: 100px;
+}
+
+
+
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,81 @@
 <template>
-<div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="d-flex">
-          <button @click.prevent= "LogoutUser" class="btn btn-outline-success" type="submit">Logout</button>
-        </form>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex float-right">
+            <button
+              @click.prevent="LogoutUser"
+              class="btn btn-outline-light"
+              type="submit"
+            >
+              Logout
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="card mb-3" style="max-width: 540px">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img :src="require('../../public/resources/perfil.png')" class="card-img" alt="..." />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{ userData.nombre }}</h5>
+            <p class="card-text">
+              Carlos es veterinario y por su puesto su amor por los animales es lo que más se destaca él. Le gusta el rock y en sus ratos libres se dedica a tocar la guitarra eléctrica con un grupo de amigos de la infancia.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">{{ userData.email }}</small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </nav>
-
-  <div class="container">
-    <p>{{userData.nombre}}</p>
-    <p>{{userData.email}}</p>
   </div>
-</div>
 </template>
 
 <script>
-
 export default {
   // name: 'Home',
-  data(){
-    return{
-      userData: {}
-    }
+  data() {
+    return {
+      userData: {},
+    };
   },
   methods: {
-    getUserDetails(){
-      let userData = localStorage.getItem('user');
-      let token = localStorage.getItem('jwt');
+    getUserDetails() {
+      let userData = localStorage.getItem("user");
+      let token = localStorage.getItem("jwt");
       console.log(JSON.parse(userData));
 
-      if(token){
-        this.userData= JSON.parse(userData);
+      if (token) {
+        this.userData = JSON.parse(userData);
       }
     },
-    LogoutUser(){
-      localStorage.removeItem('jwt');
-      localStorage.removeItem('user');
-      this.$router.push('/');
-    }
+    LogoutUser() {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      this.$router.push("/");
+    },
   },
-  created(){
+  created() {
     this.getUserDetails();
-  }
-}
+  },
+};
 </script>
+
+<style scoped>
+.navbar {
+  background-color: #73c6b6;
+  
+}
+.card{
+  width: auto; 
+  margin: auto auto;
+  margin-top: 50px;
+} 
+
+</style>
